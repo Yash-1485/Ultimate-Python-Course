9b50b7f212d44bc3d999e1674e580e4292e5ece5
Yash-1485 <yashparekh140805@gmail.com>
2024-09-23 11:15:05 +0530

    Added Chapter2.py

---
+++ Chapter2.py
@@ -0,0 +1,21 @@
+# a=10
+# b=20
+# print(a+b)
+
+# a=25
+# b=2
+# print(a%b)
+
+# n=input("Enter: ")
+# print(type(n))
+
+# a,b=34,80
+# print(a>b)
+
+# a=int(input("Enter first number: "))
+# b=int(input("Enter second number: "))
+# print("Average of these two numbers is: ",(a+b)/2)
+
+# n=int(input("Enter a number: "))
+# print("Square of number is:",n**2)
+# print("Square of number is:",n*n)
