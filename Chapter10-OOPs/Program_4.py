5210ae8811d68aa529ff86de388a386abbeb69d3
Yash-1485 <yashparekh140805@gmail.com>
2024-09-23 20:23:17 +0530

    Updated Program_4.py

--- Chapter10-OOPs/Program_4.py
+++ Chapter10-OOPs/Program_4.py
@@ -1,7 +1,16 @@
+from random import randint as rnd
 class Train:
-    def bookTickets(self):
-        pass
+    def __init__(self,trainNo):
+        self.trainNo=trainNo
+    def bookTickets(self,fro,to):
+        self.fro=fro
+        self.to=to
+        print("Train - {0} is booked for {1} to {2}.".format(self.trainNo,fro,to))
     def getStatus(self):
-        pass
+        print(f"Train - {self.trainNo} is ready to arrive.")
     def getFareInfo(self):
-        pass
+        print("Train - {0} is booked for {1} to {2} is Rs.{3}.".format(self.trainNo,self.fro,self.to,rnd(1,3000)))
+t=Train("135243")
+t.bookTickets("Kalupur","Mumbai")
+t.getStatus()
+t.getFareInfo()
