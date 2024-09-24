41adadcacbaa0fecb254a0f723ea30b5330d1eb4
Yash-1485 <yashparekh140805@gmail.com>
2024-09-24 10:00:42 +0530

    Added Property Decoraters Examples &  Constructor Overloading Example

--- Chapter11-Inheritance & All/Chapter11.py
+++ Chapter11-Inheritance & All/Chapter11.py
@@ -1,3 +1,4 @@
+from multipledispatch import dispatch
 #Single Inheritance
 # class Parent:
 #     a=10
@@ -106,15 +107,86 @@
 # print(f"The value of instance attribute a is {t.a}")
 
 #Property decoraters and getters and setters
-class Test:
-    @property
-    def name(self):
-        return self.ename
+#Example 1
+# class Test:
+#     @property
+#     def name(self):
+#         return self.ename
     
-    @name.setter
-    def name(self,value):
-        self.ename=value
-t=Test()
-t.name="Yash Parekh"
-print(t.name)
-print(t.ename)
+#     @name.setter
+#     def name(self,value):
+#         self.ename=value
+# t=Test()
+# t.name="Yash Parekh"
+# print(t.name)
+# print(t.ename)
+
+#Example 2
+# class Employee:
+#     # ename=""
+#     # companyName=""
+#     # salary=""
+    
+#     @property #Decorator
+#     def getName(self):#getter for ename
+#         return self.ename
+    
+#     @getName.setter#setter for ename
+#     def setName(self,name):
+#         self.ename=name
+
+#     @getName.deleter#deleter for ename
+#     def deleteName(self):
+#         del self.ename
+# emp=Employee()
+# emp.setName="Yash Parekh"
+# print(emp.getName)#Getter
+# print(emp.ename)#Attribute
+# # del emp.deleteName
+# # del emp.ename
+# # print(emp.ename)
+
+#Example 3 - With Constructor Overloading
+class Student:
+    @dispatch()
+    def __init__(self):
+        pass
+
+    @dispatch(str,str,int)
+    def __init__(self,name,course,marks):#*args
+        self.name=name
+        self.course=course
+        self.marks=marks
+
+    @property
+    def getName(self):
+        return self.name
+    @getName.setter
+    def setName(self,name):
+        self.name=name
+
+    @property
+    def getCourse(self):
+        return self.course
+    @getCourse.setter
+    def setCourse(self,course):
+        self.course=course
+
+    @property
+    def getMarks(self):
+        return self.marks
+    @getMarks.setter
+    def setMarks(self,marks):
+        self.marks=marks
+
+# student=Student("Yash","CE",100)
+# print(student.getName,student.getCourse,student.getMarks)
+# student.setName="Mohan"
+# print(student.getName,student.getCourse,student.getMarks)
+
+# st=Student()
+# st.setName="Yash Parekh"
+# st.setCourse=""
+# st.setMarks=0
+# print(st.name)
+# print(st.getName,st.getCourse,st.getMarks)
