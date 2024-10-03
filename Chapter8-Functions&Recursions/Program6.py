a6ad24761db73c0f2a7fea5f87471401adc6d101
Yash-1485 <yashparekh140805@gmail.com>
2024-10-03 08:00:40 +0530

    Added Program6.py to Chapter8

--- Chapter8-Functions&Recursions/Program6.py
+++ Chapter8-Functions&Recursions/Program6.py
@@ -1,4 +1,12 @@
 # Write a python function to remove a given word from a list and strip it at the same 
 # time.
+ls=['Parekh','Yash','Arun','Pooja','Ila','Hitarth']
+# ls=["Harry", "Rohan", "Shubham", "an"]
+def rem(list,word):
+    new_list=[]
+    for item in list:
+        if item!=word:
+            new_list.append(item.strip(word))
+    return new_list
 
-# list=['lorem23  ','   hello','  hi  ']
+# print(rem(ls,'h'))
